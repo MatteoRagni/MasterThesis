a869c0b58cfd02f9d61feb92f1b75a6768c4c4b1
MatteoRagni <matteo.ragni.it@gmail.com>
2014-10-13 16:15:30 +0200

    immagini finali quasi completate

--- ch4/tikz/compile.bat
+++ ch4/tikz/compile.bat
@@ -1,17 +1,35 @@
 del 3Dplot.pdf
 del state.pdf
 del searching.pdf
+del ParzenWindow.pdf
+del RangeFinder.pdf
 
 lualatex 3Dplot.tex
 lualatex state.tex
 lualatex searching.tex
+lualatex ParzenWindow.tex
+lualatex RangeFinder.tex
 
 del 3Dplot.log
 del 3Dplot.synctex.gz
 del 3Dplot.aux
+
 del state.log
 del state.synctex.gz
 del state.aux
+
 del searching.log
 del searching.synctex.gz
 del searching.aux
+
+del ParzenWindow.log
+del ParzenWindow.synctex.gz
+del ParzenWindow.aux
+
+del RangeFinder.log
+del RangeFinder.synctex.gz
+del RangeFinder.aux
+
+cd ../..
+compile.bat
+pause
