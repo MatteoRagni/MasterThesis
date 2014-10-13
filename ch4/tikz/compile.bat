1dfbd18b448e04f14ee6250a5318580ba166e5a4
MatteoRagni <matteo.ragni.it@gmail.com>
2014-10-13 03:17:09 +0200

    Riedizione completa delle figure di ch4, mancano le ultime 2

---
+++ ch4/tikz/compile.bat
@@ -0,0 +1,17 @@
+del 3Dplot.pdf
+del state.pdf
+del searching.pdf
+
+lualatex 3Dplot.tex
+lualatex state.tex
+lualatex searching.tex
+
+del 3Dplot.log
+del 3Dplot.synctex.gz
+del 3Dplot.aux
+del state.log
+del state.synctex.gz
+del state.aux
+del searching.log
+del searching.synctex.gz
+del searching.aux
